87020d88e397bd301f4d59b72ecb22aaa35c9406
Paulo <pmarsiglia93@gmail.com>
2022-01-18 22:08:17 -0300

    Subindo atividade de blog pessoal

---
+++ blogPessoal/src/main/java/org/generation/blog_pessoal/controller/PostagemController.java
@@ -0,0 +1,30 @@
+package org.generation.blog_pessoal.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import org.generation.blog_pessoal.model.Postagem;
+import org.generation.blog_pessoal.repository.PostagemRepository;
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin("*")
+public class PostagemController {
+	
+	@Autowired
+	private PostagemRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Postagem>> GetAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}	
+	
+
+}
